e9f36d7ff6a3994861cb50388dcedf08c4808d72
lassepe <lasse.peters@mailbox.org>
2020-09-30 14:21:01 +0200

    Don't replace system image

--- precompile.jl
+++ precompile.jl
@@ -7,6 +7,6 @@ include("./global_package_setup.jl")
 create_sysimage(
     [pkg.name for pkg in global_package_setup],
     precompile_execution_file = "compile_commands.jl",
-    #sysimage_path = "$(VERSION)-custom.sysimg.so",
-    replace_default = true,
+    sysimage_path = "$(VERSION)-dev.sysimg.so",
+    replace_default = false,
 )
