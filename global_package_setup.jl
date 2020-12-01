a02363d6d240803851a85cf4256fbe9b75217880
lassepe <lasse.peters@mailbox.org>
2020-12-01 14:43:23 +0100

    Add ElectronDisplay

--- global_package_setup.jl
+++ global_package_setup.jl
@@ -1,25 +1,20 @@
 const global_package_setup = [
     (name = :BenchmarkTools, compile_commands = quote
-            BenchmarkTools.@benchmark sum(1:10)
-        end),
+        BenchmarkTools.@benchmark sum(1:10)
+    end),
     (name = :Debugger, compile_commands = quote end),
+    (name = :ElectronDisplay, compile_commands = quote end),
     (name = :OhMyREPL, compile_commands = quote
-            OhMyREPL.enable_autocomplete_brackets(true)
-        end),
+        OhMyREPL.enable_autocomplete_brackets(true)
+    end),
     (name = :Revise, compile_commands = quote end),
-    (
-        name = :VegaLite,
-        compile_commands = quote
-            data = (; x = rand(10), y = rand(10))
-            VegaLite.@vlplot(:point, x = data.x, y = data.y)
-        end,
-    ),
-    (
-        name = :Plots,
-        compile_commands = quote
-            Plots.plot(rand(10), rand(10))
-            Plots.scatter(rand(10), rand(10))
-            Plots.histogram(rand(10))
-        end
-    )
+    (name = :VegaLite, compile_commands = quote
+        data = (; x = rand(10), y = rand(10))
+        VegaLite.@vlplot(:point, x = data.x, y = data.y)
+    end),
+    (name = :Plots, compile_commands = quote
+        Plots.plot(rand(10), rand(10))
+        Plots.scatter(rand(10), rand(10))
+        Plots.histogram(rand(10))
+    end),
 ]
