8786b0c544024813e99ada1e8ce2c35150f18816
lassepe <lasse.peters@mailbox.org>
2021-04-29 12:21:38 +0200

    Add InteractiveErrors

--- config/package_setup.jl
+++ config/package_setup.jl
@@ -1,10 +1,11 @@
 const package_setup = [
-    (name = :BenchmarkTools, compile_commands = quote
-        BenchmarkTools.@benchmark sum(1:10)
-    end),
-    (name = :Debugger, compile_commands = quote end),
+    (name = :Revise, compile_commands = quote end),
     (name = :OhMyREPL, compile_commands = quote
         OhMyREPL.enable_autocomplete_brackets(true)
     end),
-    (name = :Revise, compile_commands = quote end)
+    (name = :InteractiveErrors, compile_commands = quote end),
+    (name = :Debugger, compile_commands = quote end),
+    (name = :BenchmarkTools, compile_commands = quote
+        BenchmarkTools.@benchmark sum(1:10)
+    end),
 ]
