3966a383b25c163d26fe617af253b4f39fb3a471
lassepe <lasse.peters@mailbox.org>
2021-08-29 14:58:36 +0200

    Use Infiltrator instead of Debugger

--- config/package_setup.jl
+++ config/package_setup.jl
@@ -3,9 +3,8 @@ const package_setup = [
     (name = :OhMyREPL, compile_commands = quote
         OhMyREPL.enable_autocomplete_brackets(true)
     end),
-    (name = :InteractiveErrors, compile_commands = quote end),
-    (name = :Debugger, compile_commands = quote end),
     (name = :BenchmarkTools, compile_commands = quote
         BenchmarkTools.@benchmark sum(1:10)
     end),
+    (name = :Infiltrator, compile_commands = quote end),
 ]
