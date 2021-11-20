506c7ed998079aa17c24082852d790444e103294
lassepe <lasse.peters@mailbox.org>
2021-11-20 13:43:24 +0100

    Fix for new PackageCompiler version

--- src/precompile.jl
+++ src/precompile.jl
@@ -7,6 +7,5 @@ include("../config/package_setup.jl")
 create_sysimage(
     [pkg.name for pkg in package_setup],
     precompile_execution_file = "src/compile_commands.jl",
-    sysimage_path = "builds/$(VERSION)-dev.sysimg.so",
-    replace_default = false,
+    sysimage_path = "builds/$(VERSION)-dev.sysimg.so"
 )
