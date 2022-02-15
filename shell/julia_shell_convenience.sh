fa26fca5887ae6f7d1f1f4ccadaee201153e1e39
lassepe <lasse.peters@mailbox.org>
2022-02-15 10:25:39 +0100

    Disable depwarn

--- shell/julia_shell_convenience.sh
+++ shell/julia_shell_convenience.sh
@@ -5,7 +5,7 @@ SYSIMGPRECOMPILE_ROOT_DIR="$(realpath $(dirname ${(%):-%N})/..)"
 
 # NOTE: MESA_LOADER_DRIVER_OVERRIDE=i965 is a fix for https://github.com/JuliaPlots/Makie.jl/issues/630
 # -> This can be avoided by launching Julia as `optirun julia`
-alias j="MESA_LOADER_DRIVER_OVERRIDE=i965 JULIA_NUM_THREADS=10 julia --depwarn=yes"
+alias j="MESA_LOADER_DRIVER_OVERRIDE=i965 JULIA_NUM_THREADS=10 julia"
 alias jp="j --project"
 function jd {
   julia_version=$(julia -e "println(VERSION)" |  cut -d' ' -f3)
