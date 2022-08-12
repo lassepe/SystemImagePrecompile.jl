1f9272e45b1692d60edf85ac1f461b5932980ac0
lassepe <lasse.peters@mailbox.org>
2022-08-12 21:23:01 +0200

    Add packages in one go

--- src/install_packages.jl
+++ src/install_packages.jl
@@ -5,8 +5,6 @@ import Pkg
 Pkg.add("PackageCompiler")
 
 include("../config/package_setup.jl")
-for pkg_setup in package_setup
-    Pkg.add(string(pkg_setup.name))
-end
+Pkg.add([string(pkg_setup.name) for pkg_setup in package_setup])
 
 Pkg.update()
