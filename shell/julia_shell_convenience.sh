fb8bc74bd1c2071a20391629fcda1fdc0815cc77
lassepe <lasse.peters@mailbox.org>
2022-08-12 11:58:02 +0200

    Set juliaup channel based on environment variable for direnv

--- shell/julia_shell_convenience.sh
+++ shell/julia_shell_convenience.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
 # TODO: this only works in zsh
-JULIA_SYSIMAGE_LIB_DIR="$(realpath $(dirname ${(%):-%N})/../builds)"
-
+export JULIA_SYSIMAGE_LIB_DIR="$(realpath $(dirname ${(%):-%N})/../builds)"
+export JULIA_BIN=$(which julia)
 # NOTE: MESA_LOADER_DRIVER_OVERRIDE=i965 is a fix for https://github.com/JuliaPlots/Makie.jl/issues/630
-alias j="julia"
+function julia {
+  $JULIA_BIN $JULIAUP_CHANNEL $@
+}
+
+alias j=julia
 alias jp="j --project"
 function jd {
   julia_version=$(julia -e "println(VERSION)" |  cut -d' ' -f3)
