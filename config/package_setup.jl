a50f933af6b7b1439d9d7b7ce7b9eaf7ba8ea339
lassepe <lasse.peters@mailbox.org>
2022-08-16 16:04:54 +0200

    Include JuliaSyntax and clean up

--- config/package_setup.jl
+++ config/package_setup.jl
@@ -1,15 +1,18 @@
 const package_setup = [
-    (name = :Revise, compile_commands = quote end),
-    (name = :OhMyREPL, compile_commands = quote
+    (name=:Revise, compile_commands=quote end),
+    (name=:OhMyREPL, compile_commands=quote
         OhMyREPL.enable_autocomplete_brackets(true)
     end),
-    (name = :BenchmarkTools, compile_commands = quote
+    (name=:BenchmarkTools, compile_commands=quote
         BenchmarkTools.@benchmark sum(1:10)
     end),
-    (name = :Infiltrator, compile_commands = quote end),
-    (name = :GLMakie, compile_commands = quote
+    (name=:Infiltrator, compile_commands=quote end),
+    (name=:GLMakie, compile_commands=quote
         GLMakie.scatter([1]) |> display
     end),
-    (name = :TestEnv, compile_commands = quote end),
-    (name = :Symbolics, compile_commands = quote end)
+    (name=:TestEnv, compile_commands=quote end),
+    (name=:Symbolics, compile_commands=quote end),
+    (name=:JuliaSyntax, compile_commands=quote
+        Base.include(@__MODULE__(), joinpath(pkgdir(JuliaSyntax), "sysimage", "precompile_exec.jl"))
+    end)
 ]
