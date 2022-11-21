be95ac780b65af965b08e26cfdb7efbe39ca0f1f
lassepe <lasse.peters@mailbox.org>
2022-11-21 15:39:43 +0100

    Formatting

--- config/package_setup.jl
+++ config/package_setup.jl
@@ -1,21 +1,21 @@
 const package_setup = [
-    (name = :Revise, compile_commands = quote end),
-    (name = :JET, compile_commands = quote
+    (name=:Revise, compile_commands=quote end),
+    (name=:JET, compile_commands=quote
         JET.report_package("JET")
     end),
-    (name = :OhMyREPL, compile_commands = quote
+    (name=:OhMyREPL, compile_commands=quote
         OhMyREPL.enable_autocomplete_brackets(true)
     end),
-    (name = :BenchmarkTools, compile_commands = quote
+    (name=:BenchmarkTools, compile_commands=quote
         BenchmarkTools.@benchmark sum(1:10)
     end),
-    (name = :Infiltrator, compile_commands = quote end),
-    (name = :GLMakie, compile_commands = quote
+    (name=:Infiltrator, compile_commands=quote end),
+    (name=:GLMakie, compile_commands=quote
         GLMakie.scatter([1]) |> display
     end),
-    (name = :TestEnv, compile_commands = quote end),
-    (name = :Symbolics, compile_commands = quote end),
-    (name = :JuliaSyntax, compile_commands = quote
+    (name=:TestEnv, compile_commands=quote end),
+    (name=:Symbolics, compile_commands=quote end),
+    (name=:JuliaSyntax, compile_commands=quote
         Base.include(@__MODULE__(), joinpath(pkgdir(JuliaSyntax), "sysimage", "precompile_exec.jl"))
     end),
 ]
