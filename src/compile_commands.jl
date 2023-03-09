c38c30bcd43e60cb5801dc5c0e40588d5996213c
lassepe <lasse.peters@mailbox.org>
2023-03-09 12:23:11 +0100

    import -> using

--- src/compile_commands.jl
+++ src/compile_commands.jl
@@ -1,6 +1,6 @@
 include("../config/package_setup.jl")
 for pkg_setup in package_setup
     @info "Generating precompile statements for $(pkg_setup.name)..."
-    eval(:(import $(pkg_setup.name)))
+    eval(:(using $(pkg_setup.name): $(pkg_setup.name)))
     eval(pkg_setup.compile_commands)
 end
