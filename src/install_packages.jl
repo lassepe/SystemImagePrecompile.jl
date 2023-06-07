0b3ae8d1fc194ffed53214580c33e9a08ad4fc5d
lassepe <lasse.peters@mailbox.org>
2023-06-07 09:26:39 +0200

    Disable auto-precompile

--- src/install_packages.jl
+++ src/install_packages.jl
@@ -1,10 +1,9 @@
 #!/usr/bin/env julia
 
+ENV["JULIA_PKG_PRECOMPILE_AUTO"] = 0
 import Pkg
-
 Pkg.add("PackageCompiler")
-
 include("../config/package_setup.jl")
 Pkg.add([string(pkg_setup.name) for pkg_setup in package_setup])
-
 Pkg.update()
+Pkg.precompile()
