ab587de077930578107c841a2885be0f6b555cd6
lassepe <lasse.peters@mailbox.org>
2023-09-02 23:47:54 +0200

    Prune aliases

--- shell/julia_shell_convenience.sh
+++ shell/julia_shell_convenience.sh
@@ -2,16 +2,9 @@
 
 # TODO: this only works in zsh
 export JULIA_SYSIMAGE_LIB_DIR="$(realpath $(dirname ${(%):-%N})/../builds)"
-export JULIA_BIN=$(which julia)
-# NOTE: MESA_LOADER_DRIVER_OVERRIDE=i965 is a fix for https://github.com/JuliaPlots/Makie.jl/issues/630
-function julia {
-  $JULIA_BIN $@
-}
 
-alias j=julia
-alias jp="j --project"
 function jd {
-  julia_version=$(julia -e "println(VERSION)" |  cut -d' ' -f3)
-  j --sysimage="$JULIA_SYSIMAGE_LIB_DIR/$(hostname)-$julia_version.so" "$@"
+  julia_version=$(julia --startup-file=no -e "println(VERSION)" |  cut -d' ' -f3)
+  julia --sysimage="$JULIA_SYSIMAGE_LIB_DIR/$(hostname)-$julia_version.so" "$@"
 }
 alias jpd="jd --project"
